26c19de239216ced83065fdd7ee09a9bfd19cc57
Jose <gccinane@gmail.com>
2020-05-07 20:42:35 -0300

    configuraçao inicial do desafio

---
+++ src/pages/Main/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, TextInputEndEditingEventData } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Main(){
+  return <Text>
+    deus
+  </Text>
+}
+
